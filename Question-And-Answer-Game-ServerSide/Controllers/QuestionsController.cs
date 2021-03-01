8d5eed691256db7ab7cb66e43a438277de72c1a1
mohammad st <mohammad.sayartehrani@yahoo.com>
2021-03-02 02:13:15 +0330

    Add project files.

---
+++ Question-And-Answer-Game-ServerSide/Controllers/QuestionsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Question_And_Answer_Game_ServerSide.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuestionsController : ControllerBase
+    {
+        [HttpPost]
+        public void Post([FromBody] Models.Question question)
+        {
+
+        }
+    }
+}
---
+++ Question-And-Answer-Game-ServerSide/Models/Question.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Question_And_Answer_Game_ServerSide.Models
+{
+    public class Question
+    {
+        public string Text { get; set; }
+    }
+}

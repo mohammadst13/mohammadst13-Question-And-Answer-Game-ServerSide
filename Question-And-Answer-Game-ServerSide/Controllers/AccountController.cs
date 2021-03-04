86a8bc045060a6b62f1a1f5ab09ee89a26f1a939
mohammad st <mohammad.sayartehrani@yahoo.com>
2021-03-04 16:42:35 +0330

    add post login and create token method

--- Question-And-Answer-Game-ServerSide/Controllers/AccountController.cs
+++ Question-And-Answer-Game-ServerSide/Controllers/AccountController.cs
@@ -43,6 +43,26 @@ namespace Question_And_Answer_Game_ServerSide.Controllers
 
             await signInManager.SignInAsync(user, isPersistent: false);
 
+            return Ok(CreateToken(user));
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] Credentials credentials)
+        {
+            var result = await signInManager.PasswordSignInAsync(credentials.Email, credentials.Password, false, false);
+
+            if (!result.Succeeded)
+            {
+                return BadRequest();
+            }
+
+            var user = await userManager.FindByEmailAsync(credentials.Email);
+
+            return Ok(CreateToken(user));
+        }
+
+        string CreateToken(IdentityUser user)
+        {
             var claims = new Claim[]
             {
                 new Claim(JwtRegisteredClaimNames.Sub, user.Id)
@@ -52,7 +72,7 @@ namespace Question_And_Answer_Game_ServerSide.Controllers
             var signingCredentials = new SigningCredentials(signingKey, SecurityAlgorithms.HmacSha256);
 
             var jwt = new JwtSecurityToken(signingCredentials: signingCredentials, claims: claims);
-            return Content(new JwtSecurityTokenHandler().WriteToken(jwt));
+            return new JwtSecurityTokenHandler().WriteToken(jwt);
         }
     }
 }
